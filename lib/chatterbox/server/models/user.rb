628e6a48b082f87b442b85a2f055d2f603f162b7
Tim Popham <popham@uw.edu>
2012-01-13 15:50:37 -0800

    Working toward MongoDb model archetype.

--- lib/chatterbox/server/models/user.rb
+++ lib/chatterbox/server/models/user.rb
@@ -1,16 +1,74 @@
-require 'mongo'
+require './mongo_db_model'
 require './peer_opens_policy'
 
-class User
-  def initialize(id=nil)
-    if id
-      @default_peer_opens_policy = 
-      @peer_opens_policies = 
-    else
-      @default_peer_opens_policy = PeerOpensPolicy.Ask
-      @peer_opens_policies = Array.new
+class User < MongoDbModel
+  private_class_method :new
+
+  def initialize user
+    @user_id = user[:_id]
+    @username = user[:username]
+    @default_peer_opens_policy = user[:peer_opens_policy][:default]
+    @peer_opens_policies = user[:peer_opens_policy][:individuals]
+  end
+
+  def self.create(username, options={}, peer_opens_policy={})
+    fill_peer_opens_policy peer_opens_policy
+    user = {
+      username: username,
+      peer_opens_policy: peer_opens_policy
+    }
+    user[:_id] = options[:user_id] if options[:user_id]
+    self.users.insert(user, safe: true)
+    new user
+  end
+
+  def self.load(user_id)
+    user = self.users.find_one({_id: user_id})
+    if user
+      @user_id = user_id
+      @username = user[:username]
+      @default_peer_opens_policy = user[:peer_opens_policy][:default]
+      @peer_opens_policies = user[:peer_opens_policy][:individuals]
     end
   end
 
-  #more precise constructors built custom for particular use cases
+  def self.lookup_id(username)
+    self.users.find_one({username: username})['_id']
+  end
+
+  private
+  def self.fill_peer_opens_policy policy
+    policy[:default] = PeerOpensPolicy.Accept if not policy[:default]
+    policy[:individuals] = {} if not policy[:individuals]
+    policy
+  end
+
+  def self.users
+    get_collection 'users'
+  end
+
+  def self.index
+    users.create_index(:username, {unique: true})
+  end
+
+  def self.drop
+    users.drop
+  end
 end
+
+=begin Archetypal MongoDb Data Structure:
+{
+  user_id: [Some ID],
+  username: [Some Handle],
+  peer_opens_policy:
+    {
+      default: [Default Policy],
+      individuals:
+        {
+          [Some ID]: [Individual Policy],
+          [Some ID]: [Individual Policy],
+          ...
+        }
+    }
+}
+=end
