2f49a75ca39bccb9f2f2d9d9fb6792d4a8a3bc82
Tim Popham <popham@uw.edu>
2012-01-14 20:05:25 -0800

    PeerOpensPolicy with validator and named constants.

---
+++ lib/chatterbox/server/models/peer_opens_policy.rb
@@ -0,0 +1,29 @@
+require 'set'
+
+module Chatterbox
+  class PeerOpensPolicy
+    @@Verify = 1
+    @@Accept = 2
+
+    def self.Verify
+      @@Verify
+    end
+
+    def self.Accept
+      @@Accept
+    end
+
+    def self.valid? policy
+      valid_keys = Set[:default, :individuals]
+      valid_values = Set[@@Verify, @@Accept]
+      policy.each_pair do |key,value|
+        return false if not valid_keys.member? key
+        return false if not valid_values.member? value
+      end
+      true
+    end
+  end
+
+  class InvalidPeerOpensPolicy
+  end
+end
