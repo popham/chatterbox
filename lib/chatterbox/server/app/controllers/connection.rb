5e3621585b9d965436544b9b243fadb13f275579
Tim Popham <popham@uw.edu>
2012-01-15 23:11:53 -0800

    Connection restubbed.

---
+++ lib/chatterbox/server/app/controllers/connection.rb
@@ -0,0 +1,15 @@
+require 'chatterbox/server/app/controllers/command_handlers/open'
+
+module Chatterbox
+  module Controller
+    class Connection
+      @@Open = 1
+      @@Uninitialized = 2
+      @@Closed = 4
+
+      def initialize
+
+      end
+    end
+  end
+end
